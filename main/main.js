e894d56b9224d4cfb81585643628d9097dbd8277
LiMiaoJing <466020917@qq.com>
2017-05-30 14:30:08 +0800

    string-to-number get sum

--- main/main.js
+++ main/main.js
@@ -1,3 +1,18 @@
-module.exports = function main() {
-  // Write your cade here
-};
+module.exports = function main(s) {
+  /*Write your cade here
+  var length = s.length
+  var sum = 0;
+  for(var i = 0; i<length; i++){
+    sum = sum + Number(console.log(s.charCode80At(i)))*/
+    var sum = 0
+    var num = Number(s)
+    var arr = new Array()
+    for(var i = 0; i<s.length; i++){
+      arr[i] = num%10
+      num = Math.floor(num/10)
+    }
+    for(var j = 0; j<s.length; j++){
+      sum = sum + arr[j]
+    }
+    return sum
+  };
